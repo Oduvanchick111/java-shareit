1fd066f6de48b4026b08067328a03a767b89cd26
Oduvnachick111 <pboltinskiy@yandex.ru>
2025-08-01 18:07:10 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
