917d752aa29ccad10ce09379a85bff192aad37ba
Oduvanchick111 <pboltinsky@yandex.ru>
2025-08-11 00:47:05 +0300

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -10,10 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.item.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.RequestDto;
-
-import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/requests")
